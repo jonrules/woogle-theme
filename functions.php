c22605298301e5d97b166ff331ee59c798c34daa
Jonathan Baltazar <jonrules@gmail.com>
2015-06-24 12:10:23 -0500

    copy templates for archive, comments, page, sidebar, single

--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php
 
+define( 'WOOGLE_VERSION', '1.0.0' );
+
 function woogle_scripts() {
 	$theme_directory = get_template_directory_uri();
-	wp_enqueue_style( 'materialize', $theme_directory . '/css/materialize.min.css' );
-	wp_enqueue_script( 'materialize', $theme_directory . '/js/materialize.min.js', array( 'jquery' ) );
+	// Materialize
+	wp_enqueue_style( 'materialize', $theme_directory . '/css/materialize.min.css', array(), WOOGLE_VERSION );
+	wp_enqueue_script( 'materialize', $theme_directory . '/js/materialize.min.js', array( 'jquery' ), WOOGLE_VERSION );
+	// Theme
+	wp_enqueue_style( 'woogle', $theme_directory . '/style.css', array(), WOOGLE_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'woogle_scripts' );
