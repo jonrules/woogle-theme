a2bcecb824a36aecf546e7c358e844fdf3c06216
Jonathan Baltazar <jonrules@gmail.com>
2015-06-24 12:21:47 -0500

    add theme features support and widget areas

--- functions.php
+++ functions.php
@@ -11,3 +11,73 @@ function woogle_scripts() {
 	wp_enqueue_style( 'woogle', $theme_directory . '/style.css', array(), WOOGLE_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'woogle_scripts' );
+
+function woogle_setup() {
+
+	/*
+	 * Make theme available for translation.
+	* Translations can be filed in the /languages/ directory.
+	* If you're building a theme based on twentyfifteen, use a find and replace
+	* to change 'twentyfifteen' to the name of your theme in all the template files
+	*/
+// 	load_theme_textdomain( 'woogle', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	* By adding theme support, we declare that this theme does not use a
+	* hard-coded <title> tag in the document head, and expect WordPress to
+	* provide it for us.
+	*/
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	*
+	* See: https://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	*/
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 825, 510, true );
+
+	// This theme uses wp_nav_menu() in two locations.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu',      'woogle' ),
+		'social'  => __( 'Social Links Menu', 'woogle' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	* to output valid HTML5.
+	*/
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	*
+	* See: https://codex.wordpress.org/Post_Formats
+	*/
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+	) );
+}
+add_action( 'after_setup_theme', 'woogle_setup' );
+
+/**
+ * Register widget areas.
+ */
+function woogle_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Widget Area', 'woogle' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Add widgets here to appear in your sidebar.', 'woogle' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'woogle_widgets_init' );
