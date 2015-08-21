8e714dc502541a8799aae77afd4b4b2940f00528
Jonathan Baltazar <jonrules@gmail.com>
2015-08-21 14:30:31 -0400

    add stacked-images

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-$(".button-collapse").sideNav();
+jQuery(function ($) {
+        $(".button-collapse").sideNav();
+
+        $('.stacked-images').each(function () {
+		var $images = $(this).find('img');
+		var animateClass = 'animated bounceInDown';
+		var i = 0;
+        	setInterval(function () {
+			var j = (i + 1) % $images.length;
+			$images.not(':eq(' + i + ')').css('visibility', 'hidden').removeClass(animateClass);
+			$images.eq(i).css('z-index', '1');
+			$images.eq(j).css('z-index', '1000').css('visibility', 'visible').addClass(animateClass);
+			i = j;
+		}, 2500);
+	});
+});
