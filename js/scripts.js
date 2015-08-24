6ee4205031c8625a68dbf8353fa2801014a3546f
Jonathan Baltazar <jonrules@gmail.com>
2015-08-24 18:15:08 -0400

    reduce z-index of stacked-images

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ jQuery(function ($) {
 
         $('.stacked-images').each(function () {
 		var $images = $(this).find('img');
-		var animateClass = 'animated bounceInDown';
+		var animateClass = 'animated bounceInUp';
 		var i = 0;
         	setInterval(function () {
 			var j = (i + 1) % $images.length;
 			$images.not(':eq(' + i + ')').css('visibility', 'hidden').removeClass(animateClass);
 			$images.eq(i).css('z-index', '1');
-			$images.eq(j).css('z-index', '1000').css('visibility', 'visible').addClass(animateClass);
+			$images.eq(j).css('z-index', '100').css('visibility', 'visible').addClass(animateClass);
 			i = j;
 		}, 2500);
 	});
