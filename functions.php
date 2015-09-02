cadcf6f32860e966ab5cc370af1e9d284b67ea5d
Jonathan Baltazar <jonrules@gmail.com>
2015-09-02 17:32:39 -0400

    add sidebar download-sidebar

--- functions.php
+++ functions.php
@@ -97,6 +97,16 @@ function woogle_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+                'name'          => __( 'Download Sidebar', 'woogle' ),
+                'id'            => 'download-sidebar',
+                'description'   => __( 'Add widgets here to appear in your download page sidebar.', 'woogle' ),
+                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</aside>',
+                'before_title'  => '<h2 class="widget-title">',
+                'after_title'   => '</h2>',
+        ) );
 }
 add_action( 'widgets_init', 'woogle_widgets_init' );
 
