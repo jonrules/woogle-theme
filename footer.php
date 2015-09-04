4ee447e633d16f93241b59504306ca61f5467c15
Jonathan Baltazar <jonrules@gmail.com>
2015-09-04 17:21:16 -0500

    display footer menu

--- footer.php
+++ footer.php
@@ -18,8 +18,7 @@
 				<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>  -->
 			</div>
 			<div class="col l4 offset-l2 s12">
-				<!--  <h5 class="white-text">Links</h5>
-				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?> -->
+				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
 			</div>
 		</div>
 	</div>
