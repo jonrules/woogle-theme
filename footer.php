ba8312c48867aa727fc0f8d4aac46ece28cc012a
Jonathan Baltazar <jonrules@gmail.com>
2015-09-04 17:22:44 -0500

    white-text on footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 <footer id="footer" class="page-footer">
 	<div class="container">
-		<div class="row">
+		<div class="row white-text">
 			<div class="col l6 s12">
 				<!-- <h5 class="white-text">Footer Content</h5> 
 				<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>  -->
